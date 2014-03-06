38c9cb984656121197216c126a928554a3f4a19e
Hieu Phan <hnphan@colby.edu>
2014-03-06 06:09:16 -0500

    Added pull to refresh

--- app/model/Meal.js
+++ app/model/Meal.js
@@ -1,6 +1,6 @@
 Ext.define('WTF.model.Meal', {
 	extend: 'Ext.data.Model',
 	config: {
-		fields: ['date','dining_hall', 'meal_name', 'menu']
+		fields: ['id','date','dining_hall', 'meal_name', 'menu']
 	} 
 });
--- app/view/Menu.js
+++ app/view/Menu.js
@@ -1,3 +1,29 @@
+var timeAgoInWords = function(date) {
+    try {
+        var now = Math.ceil(Number(new Date()) / 1000),
+            dateTime = Math.ceil(Number(new Date(date)) / 1000),
+            diff = now - dateTime,
+            str;
+
+        if (diff < 60) {
+            return String(diff) + ' seconds ago';
+        } else if (diff < 3600) {
+            str = String(Math.ceil(diff / (60)));
+            return str + (str == "1" ? ' minute' : ' minutes') + ' ago';
+        } else if (diff < 86400) {
+            str = String(Math.ceil(diff / (3600)));
+            return str + (str == "1" ? ' hour' : ' hours') + ' ago';
+        } else if (diff < 60 * 60 * 24 * 365) {
+            str = String(Math.ceil(diff / (60 * 60 * 24)));
+            return str + (str == "1" ? ' day' : ' days') + ' ago';
+        } else {
+            return Ext.Date.format(new Date(date), 'jS M \'y');
+        }
+    } catch (e) {
+        return '';
+    }
+};
+
 Ext.define('WTF.view.Menu', {
     extend: 'Ext.tab.Panel',
     requires: ['Ext.TitleBar', 'Ext.dataview.List'],
@@ -22,6 +48,9 @@ Ext.define('WTF.view.Menu', {
                 },
                 {
                     xtype: 'list',
+                    plugins: [
+                        { xclass: 'Ext.plugin.PullRefresh' }
+                    ],
                     store: 'BreakfastMenus',
                     itemTpl: '<h1>{dining_hall}</h1><br/> {menu}',
                     itemCls: 'menu',
@@ -39,6 +68,9 @@ Ext.define('WTF.view.Menu', {
                 },
                 {
                     xtype: 'list',
+                    plugins: [
+                        { xclass: 'Ext.plugin.PullRefresh' }
+                    ],
                     store: 'LunchMenus',
                     itemTpl: '<h1>{dining_hall}</h1><br/> {menu}',
                     itemCls: 'menu',
@@ -60,6 +92,9 @@ Ext.define('WTF.view.Menu', {
                 },
                 {
                     xtype: 'list',
+                    plugins: [
+                        { xclass: 'Ext.plugin.PullRefresh' }
+                    ],
                     store: 'DinnerMenus',
                     itemTpl: '<h1>{dining_hall}</h1><br/> {menu}',
                     itemCls: 'menu',
